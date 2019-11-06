47ade20993ee17dd7cc0523a1599739f8699b75e
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-06 15:01:01 -0800

    created reducer and set up createStore from redux. wrapped app in provider and passed store into provider as prop

--- src/index.js
+++ src/index.js
@@ -5,5 +5,15 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducer, initialState } from './reducers/reducer';
+
+const store = createStore(reducer, initialState)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>,
+ rootElement);
---
+++ src/reducers/reducer.jsx
@@ -0,0 +1,5 @@
+export const initialState =[];
+
+export function reducer(state, action) {
+  return state;
+}
